aea15d06a0b02a91430d81b7868c1997af46bcbb
tonynito <student-03-bbcdbec55d82@qwiklabs.net>
2026-02-01 11:37:37 +0000

    add yellow, remove blue for dev

--- main.go
+++ main.go
@@ -25,17 +25,18 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/blue", blueHandler)
+//	http.HandleFunc("/blue", blueHandler)
 	http.HandleFunc("/red", redHandler)
+	http.HandleFunc("/yellow", yellowHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func blueHandler(w http.ResponseWriter, r *http.Request) {
-	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{0, 0, 255, 255}}, image.ZP, draw.Src)
-	w.Header().Set("Content-Type", "image/png")
-	png.Encode(w, img)
-}
+// func blueHandler(w http.ResponseWriter, r *http.Request) {
+//	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+//	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{0, 0, 255, 255}}, image.ZP, draw.Src)
+//	w.Header().Set("Content-Type", "image/png")
+//	png.Encode(w, img)
+// }
 
 func redHandler(w http.ResponseWriter, r *http.Request) {
 	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
@@ -43,3 +44,13 @@ func redHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/png")
 	png.Encode(w, img)
 }
+
+func yellowHandler(w http.ResponseWriter, r *http.Request) {
+    img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+    draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 255, 0, 255}}, image.ZP, draw.Src)
+    w.Header().Set("Content-Type", "image/png")
+    if err := png.Encode(w, img); err != nil {
+        http.Error(w, "Failed to encode image", http.StatusInternalServerError)
+    }
+}
+
