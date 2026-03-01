a818512782b0086ac6080df1a52935774b94896a
tonynito <student-03-535999aa337b@qwiklabs.net>
2026-03-01 15:28:38 +0000

    add green for prod

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ import (
 func main() {
 	http.HandleFunc("/blue", blueHandler)
 	http.HandleFunc("/red", redHandler)
+	http.HandleFunc("/green", greenHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -40,6 +41,13 @@ func blueHandler(w http.ResponseWriter, r *http.Request) {
 func redHandler(w http.ResponseWriter, r *http.Request) {
 	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
 	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
+	w.Header().Set("Content-Type", "image/png")
+	png.Encode(w, img)
+}
+
+func greenHandler(w http.ResponseWriter, r *http.Request) {
+	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{0, 255, 0, 255}}, image.Point{}, draw.Src)
 	w.Header().Set("Content-Type", "image/png")
 	png.Encode(w, img)
 }
